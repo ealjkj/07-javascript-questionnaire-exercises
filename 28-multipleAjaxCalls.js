f554b3e5c96e2319b4def94a9b9f731f3d334b7f
Jorge Ballote <ealjkj@gmail.com>
2022-10-07 14:08:12 -0500

    28 now accepts errors

--- 28-multipleAjaxCalls.js
+++ 28-multipleAjaxCalls.js
@@ -2,14 +2,24 @@ var async = {
     getAll: function (urlArray, callback) {
         let promises = []; 
         for(let url of urlArray) {
-            promises.push(fetch(url));
+            promises.push(fetch(url).then(value => value.json()));
         }
 
         Promise.allSettled(promises).then(values  => {
-            const fullfilledValues = values.filter(({status}) => status === "fulfilled");
-            const context = fullfilledValues.map(({value}) => value);
+            const context = values.map(({value, reason}) => {
+                return value || reason
+            });         
             callback.call(context);
         });
     }
 }
 
+//Testing
+const urls = [
+    "https://swapi.dev/api/people/1", 
+    "https://swapi.dev/api/planets/3", 
+    "https://swapi.dev/api/starships/9", 
+    "https://swapi.dev/api/error" // This one will fail
+]
+
+async.getAll(urls, function() { console.log(this)})
